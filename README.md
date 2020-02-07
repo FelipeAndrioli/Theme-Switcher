d5b90e081cfefb6bd59f30b321a9f43fe871b0c7
Felipe Andrioli <felipeandrioli56@hotmail.com>
2020-02-07 17:09:03 -0300

    First version of the application

---
+++ README.md
@@ -0,0 +1,4 @@
+## Theme Switch Application
+
+The application was maded with React, Typescript and Styled Components to learn more
+about Typescript and Styled Components. A Javascript version will be coded later.
---
+++ Typescript/src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import Switch from 'react-switch'
+import { Container } from './styles'
+import { ThemeContext } from 'styled-components'
+import { shade } from 'polished'
+
+interface Props {
+    toggleTheme(): void
+}
+
+const Header: React.FC<Props> = ({ toggleTheme }) => {
+
+    const { colors, title } = useContext(ThemeContext)
+
+    return(
+        <Container> Theme Change Application 
+            <Switch 
+                onChange = {toggleTheme}
+                checked = {title === 'dark'}
+                checkedIcon = {false}
+                uncheckedIcon = {false}
+                height = {10}
+                width = {40}
+                handleDiameter = {20}
+                //offColor = {shade(0.15, colors.primary)}
+                offColor = "#333"
+                onColor = {colors.secundary}
+            />
+        </Container>
+    )
+}
+
+export default Header
